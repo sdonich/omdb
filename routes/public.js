ed648eb1b2887a30335aae4d29296b35a1df38aa
sdonich <sdonich@gmail.com>
2018-11-29 22:42:52 +0200

    public отреставрирован

--- routes/public.js
+++ routes/public.js
@@ -1,18 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 
-function public(contentType, req, res) {
-  const filepath = req.url.slice(1);
+function public(req, res) {
+  const extension = path.extname(req.url);
+  let contentType = '';
 
-  fs.readFile(path.resolve('public', filepath), (error, html) => {
-    if (error) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('error');
+  switch (extension) {
+    case '.html':
+      contentType = 'text/html';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    default:
+      contentType = 'text/plain';
+  }
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', contentType);
+  const filepath = req.url.slice(1);
+  const stream = fs.createReadStream(path.resolve('public', filepath));
+
+  stream.on('error', error => {
+    if (error.code === 'ENOENT') {
+      res.writeHead(404, { 'Content-type': 'text/plain' });
+      res.end('Not found');
     } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(html);
+      res.writeHead(500, { 'Content-type': 'text/plain' });
+      res.end(error.message);
     }
   });
+
+  stream.pipe(res);
 }
 
 module.exports = public;
--- server.js
+++ server.js
@@ -2,31 +2,15 @@ const http = require('http');
 const [home, public, search] = require('./routes/index');
 
 http.createServer((req, res) => {
-  let contentType;
-  
-  if (req.url.endsWith('.png')) {
-    contentType = 'image/png';
 
-  } else if (req.url.endsWith('.jpg')) {
-    contentType = 'image/jpeg';
+  if (req.url.match(/\.(html|css|js|png)$/)) {
+    public(req, res);
 
-  } else if (req.url.endsWith('.css')) {
-    contentType = 'text/css';
-
-  } else {
-    contentType = 'text/html';
-    
-  }
-
-  if (req.url === '/') {
+  } else if (req.url === '/') {
     home(res);
 
-  } else if (req.url.search(/search/) !== -1) {
+  } else if (req.url.startsWith('/search')) {
     search(req, res);
 
-  } else {
-    public(contentType, req, res);
-
   }
-
 }).listen(3000, () => console.log('Server is running'));
