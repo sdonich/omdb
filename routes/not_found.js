016cad9556fc63ebac28e287fa77c3885a050422
sdonich <sdonich@gmail.com>
2018-11-29 14:27:10 +0200

    Достигнута полная функциональность

--- routes/not_found.js
+++ routes/not_found.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-function notFound() {
+function notFound(res) {
 
+  fs.readFile(path.resolve('public', 'error.html'), 'utf-8', (error, temlate) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(temlate);
+  });
 }
 
 module.exports = notFound;
--- routes/search.js
+++ routes/search.js
@@ -2,11 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const omdb = require('.././lib/omdb');
 const render = require('.././lib/render');
+const notFound = require('./not_found');
 
 function search(req, res) {
 
   fs.readFile(path.resolve('public', 'movie.html'), 'utf-8', (error, temlate) => {
-    omdb(req, (movie) => {
+    omdb(req, (error, movie) => {
+      if (error) {
+        return notFound(res);
+      } 
+
       render(movie, temlate, (html) => {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(html);
@@ -16,3 +21,5 @@ function search(req, res) {
 }
 
 module.exports = search;
+
+// { Response: 'False', Error: 'Movie not found!' }
